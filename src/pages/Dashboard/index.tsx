692846a6832884dfa275ba45e8207c1ca3c239e4
Róger Araújo <rogeralves_90@yahoo.com.br>
2020-04-24 22:32:18 -0300

    Connecting with github api

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,6 +1,72 @@
-import React from 'react';
-import Title from './styles';
+import React, { useState, FormEvent } from 'react';
+import { FiChevronRight } from 'react-icons/fi';
+import { Title, Form, Repositories } from './styles';
+import logoImage from '../../assets/logo.svg';
+import api from '../../services/api';
 
-const Dashboard: React.FC = () => <Title>Explore Repostórios no Github</Title>;
+interface Repository {
+  full_name: string;
+  description: string;
+  owner: {
+    login: string;
+    avatar_url: string;
+  };
+}
+
+const Dashboard: React.FC = () => {
+  const [inputValue, updateInputValue] = useState('');
+  const [repositories, updateRepositories] = useState<Repository[]>([]);
+
+  async function newRepositoryAdded(
+    event: FormEvent<HTMLFormElement>
+  ): Promise<void> {
+    event.preventDefault();
+
+    const repositoryResponse = await api.get<Repository>(
+      `/repos/${inputValue}`
+    );
+
+    const newRepository = repositoryResponse.data;
+
+    updateRepositories([...repositories, newRepository]);
+    updateInputValue('');
+  }
+
+  return (
+    <>
+      <img src={logoImage} alt="Github explorer" />
+      <Title>Explore Repostórios no Github</Title>
+
+      <Form onSubmit={newRepositoryAdded}>
+        <input
+          value={inputValue}
+          onChange={(event) => updateInputValue(event.target.value)}
+          placeholder="Digite o nome do repositório."
+        />
+        <button type="submit">Pesquisar</button>
+      </Form>
+
+      <Repositories>
+        {repositories.map((repository) => (
+          <a
+            key={repository.full_name}
+            href="https://github.com/rogeraraujo90/bethehero"
+          >
+            <img
+              src={repository.owner.avatar_url}
+              alt={repository.owner.login}
+            />
+            <div>
+              <strong>{repository.full_name}</strong>
+              <p>{repository.description}</p>
+            </div>
+
+            <FiChevronRight size={20} />
+          </a>
+        ))}
+      </Repositories>
+    </>
+  );
+};
 
 export default Dashboard;
--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -1,8 +1,98 @@
 import styled from 'styled-components';
+import { shade } from 'polished';
 
 const Title = styled.h1`
   font-size: 48px;
   color: #3a3a3a;
+  margin-top: 80px;
+  max-width: 450px;
+  line-height: 56px;
 `;
 
-export default Title;
+const Form = styled.form`
+  display: flex;
+  margin-top: 40px;
+  max-width: 700px;
+
+  input {
+    flex: 1;
+    height: 70px;
+    padding: 0 24px;
+    border: 0;
+    border-radius: 5px 0 0 5px;
+    color: #3a3a3a;
+
+    &::placeholder {
+      color: #a8a8b3;
+    }
+  }
+
+  button {
+    width: 210px;
+    background: #04d361;
+    border-radius: 0 5px 5px 0;
+    color: #fff;
+    font-weight: bold;
+    border: 0;
+    transition: background-color 0.2s;
+
+    &:hover {
+      background: ${shade(0.2, '#04d361')};
+    }
+  }
+`;
+
+const Repositories = styled.div`
+  margin-top: 80px;
+  width: 700px;
+
+  a {
+    display: flex;
+    align-items: center;
+
+    background: #fff;
+    border-radius: 5px;
+    width: 100%;
+    padding: 24px;
+    text-decoration: none;
+
+    & + a {
+      margin-top: 16px;
+    }
+
+    img {
+      width: 64px;
+      height: 64px;
+      border-radius: 50%;
+    }
+
+    div {
+      flex: 1;
+      margin-left: 16px;
+
+      strong {
+        font-size: 20px;
+        color: #3d3d4d;
+      }
+
+      p {
+        font-size: 18px;
+        color: #a8a8b3;
+        margin-top: 4px;
+      }
+    }
+
+    svg {
+      margin: 0 16px;
+      color: #cbcbd6;
+    }
+
+    transition: transform 0.2s;
+
+    &:hover {
+      transform: translateX(10px);
+    }
+  }
+`;
+
+export { Title, Form, Repositories };
